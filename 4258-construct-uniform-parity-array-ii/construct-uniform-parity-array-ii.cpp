6fc0123082423d694bfb60431874a51a12dd6a8d
imyash007 <yashgupta007339@gmail.com>
2026-09-03 22:23:41 +0530

    Time: 4 ms (65.18%) | Memory: 165.8 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool uniformArray(vector<int>& A) {
         int xmin = A[0];
         bool odd = 0;
-
+        
         for (auto& x : A) {
             xmin = min(xmin, x);
             odd |= x & 1;//checks if x is odd
